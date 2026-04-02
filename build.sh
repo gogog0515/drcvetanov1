f79d0ad4be063fbff78e865ed480f77b640b23d8
D-r Cvetanov <YOUR_EMAIL@example.com>
2026-04-02 13:42:04 +0300

    Fix Render superuser creation

--- build.sh
+++ build.sh
@@ -3,5 +3,26 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py migrate
-python appointments/scripts/create_superuser.py
+python manage.py shell -c "
+import os
+from django.contrib.auth import get_user_model
+User = get_user_model()
+username = os.getenv('DJANGO_SUPERUSER_USERNAME')
+password = os.getenv('DJANGO_SUPERUSER_PASSWORD')
+email = os.getenv('DJANGO_SUPERUSER_EMAIL', '')
+if username and password:
+    user, created = User.objects.get_or_create(
+        username=username,
+        defaults={'email': email, 'is_staff': True, 'is_superuser': True},
+    )
+    user.is_staff = True
+    user.is_superuser = True
+    if email:
+        user.email = email
+    user.set_password(password)
+    user.save()
+    print('superuser ok')
+else:
+    print('superuser skipped')
+"
 python manage.py collectstatic --noinput
